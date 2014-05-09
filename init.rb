428f01e60cf503c8c6ab851bf08900df78c9c262
Szabolcs Szász <lunakid@gmail.com>
2014-05-10 01:38:19 +0200

    Package rename & version chg.

--- init.rb
+++ init.rb
@@ -1,13 +1,13 @@
 require 'redmine'
 
-require_dependency 'redmine_open_links_in_new_window/view_layouts_base_html_head_hook'
+require_dependency 'external_link_in_new_window/view_layouts_base_html_head_hook'
 
-Redmine::Plugin.register :redmine_open_links_in_new_window do
-  name 'Redmine Open Links In New Window plugin'
-  author 'akzhan.abdulin@gmail.com'
-  description 'This is the Open Links In New Window plugin for Redmine'
-  version '0.0.3'
-  url 'http://github.com/Undev/redmine_open_links_in_new_window'
-  author_url 'http://github.com/Undev'
+Redmine::Plugin.register :external_link_in_new_window do
+  name 'External Links In New Window'
+  author 'akzhan.abdulin@gmail.com, lunakid@gmail.com'
+  description 'Open external & attachment links in new browser windows.'
+  version '0.9'
+  url 'https://github.com/lunakid/redmine_external_link_in_new_window'
+  author_url 'http://github.com/lunakid'
 end
 
