b311b19b6e87a4c67e327d298e2810e56802b48e
Szabolcs Szász <lunakid@gmail.com>
2014-05-09 20:07:29 +0200

    Open only external links in new window!

--- assets/javascripts/redmine_open_links_in_new_window.js
+++ assets/javascripts/redmine_open_links_in_new_window.js
@@ -1,47 +1,41 @@
 (function()
 {
-	/**
-	 * Need to be binded to anchor on call
-	 */
-	var handleAnchor = function()
-	{
-		var pass = true;
+        /**
+         * Need to be bound to the DOM object of the relevant A element on call
+         */
+        var handleAnchor = function()
+        {
+                if (-1 != this.className.indexOf('external'))
+                {
+                        this.target = '_blank';
+                }
+        };
 
-		// Requirements
-		if (-1 != this.className.indexOf('wiki-anchor')) pass = false;
+        if (document.observe)
+        {
+                // redmine uses prototype so use it.
 
-		if (pass)
-		{
-			this.target = '_blank';
-		}
-	};
+                document.observe('dom:loaded', function()
+                {
+                        var links = $$('div.attachments a, a.external');
+                        for (var i = 0; i < links.length; i++)
+                        {
+                                handleAnchor.call(links[i]);
+                        }
+                });
+        }
+        else if (window.jQuery)
+        {
 
-	if (document.observe)
-	{
+                // redmine uses jQuery so use it.
 
-		// redmine uses prototype so use it.
-
-		document.observe('dom:loaded', function()
-		{
-			var links = $$('div.wiki a, div.attachments a, div.journal ul.details a, a.external');
-			for (var i = 0; i < links.length; i++)
-			{
-				handleAnchor.call(links[i]);
-			}
-		});
-	}
-	else if (window.jQuery)
-	{
-
-		// redmine uses jQuery so use it.
-
-		jQuery(document).ready(function()
-		{
-			jQuery('div.wiki a, div.attachments a, div.journal ul.details a, a.external').each(handleAnchor);
-		});
-	}
-	else
-	{
-		console.error('(redmine_open_links_in_new_window) JS-framework is unknown!');
-	}
+                jQuery(document).ready(function()
+                {
+                        jQuery('div.attachments a, a.external').each(handleAnchor);
+                });
+        }
+        else
+        {
+                console.error('(redmine_open_links_in_new_window) JS-framework is unknown!');
+        }
 })();
