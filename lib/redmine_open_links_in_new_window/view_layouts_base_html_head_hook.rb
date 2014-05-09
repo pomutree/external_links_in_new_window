09c6d98e68e5daa19d1e244c53ef341a6820696e
Szabolcs Szász <lunakid@gmail.com>
2014-05-10 01:41:06 +0200

    Package rename

--- lib/redmine_open_links_in_new_window/view_layouts_base_html_head_hook.rb
+++ lib/redmine_open_links_in_new_window/view_layouts_base_html_head_hook.rb
@@ -1,7 +1,7 @@
-module RedmineOpenLinksInNewWindow
+module ExternalLinkInNewWindow
   class ViewLayoutsBaseHtmlHeadHook < Redmine::Hook::ViewListener
     def view_layouts_base_html_head(context = {})
-      javascript_include_tag('redmine_open_links_in_new_window.js', :plugin => :redmine_open_links_in_new_window)
+      javascript_include_tag('handler.js', :plugin => :external_link_in_new_window)
     end
   end
 end
