776a0f5b763f98b8b42b20de7a2648bac092ccf5
Szabolcs Szász <lunakid@gmail.com>
2014-05-10 02:19:40 +0200

    Changed my mind, let it be plural again. ;)

--- lib/external_links_in_new_window/view_layouts_base_html_head_hook.rb
+++ lib/external_links_in_new_window/view_layouts_base_html_head_hook.rb
@@ -1,7 +1,7 @@
-module ExternalLinkInNewWindow
+module ExternalLinksInNewWindow
   class ViewLayoutsBaseHtmlHeadHook < Redmine::Hook::ViewListener
     def view_layouts_base_html_head(context = {})
-      javascript_include_tag('handler.js', :plugin => :external_link_in_new_window)
+      javascript_include_tag('handler.js', :plugin => :external_links_in_new_window)
     end
   end
 end
