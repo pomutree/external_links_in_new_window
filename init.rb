f54706920a6944a8784bd255cefe9c04434cf6c3
Szabolcs Szász <lunakid@gmail.com>
2014-05-10 02:17:47 +0200

    Changed my mind, let it be plural again. ;)

--- init.rb
+++ init.rb
@@ -1,13 +1,13 @@
 require 'redmine'
 
-require_dependency 'external_link_in_new_window/view_layouts_base_html_head_hook'
+require_dependency 'external_links_in_new_window/view_layouts_base_html_head_hook'
 
-Redmine::Plugin.register :external_link_in_new_window do
+Redmine::Plugin.register :external_links_in_new_window do
   name 'External Links In New Window'
   author 'akzhan.abdulin@gmail.com, lunakid@gmail.com'
   description 'Open external & attachment links in new browser windows.'
   version '0.9'
-  url 'https://github.com/lunakid/redmine_external_link_in_new_window'
+  url 'https://github.com/lunakid/redmine_external_links_in_new_window'
   author_url 'http://github.com/lunakid'
 end
 
